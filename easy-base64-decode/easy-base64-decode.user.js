958f71f9c477c35632cd98a4db52e79bfc86f85f
John Ross <jlross42@gmail.com>
2020-07-14 01:55:58 -0700

    updated with github urls

--- easy-base64-decode/easy-base64-decode.user.js
+++ easy-base64-decode/easy-base64-decode.user.js
@@ -6,6 +6,8 @@
 // @version     1.0
 // @author      turkoid
 // @description Adds icon to quickly decode base64
+// @updateURL   https://raw.githubusercontent.com/turkoid/userscripts/master/easy-base64-decode/easy-base64-decode.meta.js
+// @downloadURL https://raw.githubusercontent.com/turkoid/userscripts/master/easy-base64-decode/easy-base64-decode.user.js
 // ==/UserScript==
 
 (function () {
