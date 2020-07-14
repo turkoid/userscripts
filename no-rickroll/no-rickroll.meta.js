4102709c0f8a04928ea3236a82276abb410aa96a
John Ross <jlross42@gmail.com>
2020-07-14 02:49:08 -0700

    fix meta urls

--- no-rickroll/no-rickroll.meta.js
+++ no-rickroll/no-rickroll.meta.js
@@ -6,6 +6,6 @@
 // @version     1.0
 // @author      turkoid
 // @description Adds a Rick Astley image to links that contain the infamous youtube video
-// @updateURL   https://raw.githubusercontent.com/turkoid/userscripts/master/no-rickroll.meta.js
-// @downloadURL https://raw.githubusercontent.com/turkoid/userscripts/master/no-rickroll.user.js
+// @updateURL   https://raw.githubusercontent.com/turkoid/userscripts/master/no-rickroll/no-rickroll.meta.js
+// @downloadURL https://raw.githubusercontent.com/turkoid/userscripts/master/no-rickroll/no-rickroll.user.js
 // ==/UserScript==
