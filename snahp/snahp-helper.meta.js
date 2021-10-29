31d9790e034460521e71892ca9b296f193ba313c
John Ross <jlross42@gmail.com>
2021-10-29 10:03:22 -0700

    copy base64 script and change urls

---
+++ snahp/snahp-helper.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        Snaph Helper
+// @namespace   turkoid
+// @match       https://snahp.url/*
+// @grant       none
+// @version     1.0
+// @author      turkoid
+// @description Snaph, but gooder.
+// @updateURL   https://raw.githubusercontent.com/turkoid/userscripts/master/snahp/snahp-helper.meta.js
+// @downloadURL https://raw.githubusercontent.com/turkoid/userscripts/master/snahp/snahp-helper.user.js
+// ==/UserScript==
