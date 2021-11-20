d1f91c20d5659f571f348efa2b6c6857b7bbbc5f
John Ross <jlross42@gmail.com>
2021-11-19 18:40:46 -0800

    bump version

--- snahp/snahp-helper.user.js
+++ snahp/snahp-helper.user.js
@@ -3,7 +3,7 @@
 // @namespace   turkoid
 // @match       https://snahp.url/*
 // @grant       none
-// @version     1.3.1
+// @version     1.3.2
 // @author      turkoid
 // @description Snahp, but gooder.
 // @updateURL   https://raw.githubusercontent.com/turkoid/userscripts/master/snahp/snahp-helper.meta.js
